ef45142cdd8fb12e4d56e3a48cead4ccc9decb43
05samuk <samuelribs1308@gmail.com>
2024-03-12 13:15:58 -0300

    exercicios

---
+++ lista 3/exercicio 3/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 0;
+        while (n < 200){
+            System.out.println("verdadeiro");
+            n++;
+            if (n / 4){
+
+            }
+
+        }
+    }
+}
